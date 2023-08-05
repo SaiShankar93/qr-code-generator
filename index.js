cfd79d4594da37a572aa0db13f26b508eee32539
Sai Shankar <113768646+SaiShankar93@users.noreply.github.com>
2023-08-05 22:17:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ inquirer
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
+      // Prompt couldn't be rendered in  current environment
     } else {
       // Something went wrong
     }
